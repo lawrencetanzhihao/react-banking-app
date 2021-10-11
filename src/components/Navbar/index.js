05b7e1a789d888336dccf6354dec2c23ce7c6fe4
Lawrence Tan <lawrencetanzhihao@gmail.com>
2021-10-12 00:01:55 +0800

    Initial commit with Navbar component

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from 'react';
+import { FaBars } from 'react-icons/fa';
+import { IconContext } from 'react-icons';
+
+import {
+   HamburgerIcon,
+   Nav,
+   NavbarContainer,
+   NavBtn,
+   NavBtnLink,
+   NavItem,
+   NavLinks,
+   NavLogo, 
+   NavMenu
+} from './NavbarStyles';
+
+const Navbar = ({ toggle }) => {
+
+
+   return (
+      <>
+         <IconContext.Provider value={{ color: "#fff" }}>
+            <Nav>
+               <NavbarContainer>
+                  
+                  <NavLogo to="/">Bank</NavLogo>
+
+                  <HamburgerIcon onClick={toggle}>
+                     <FaBars />
+                  </HamburgerIcon>
+
+                  <NavMenu>
+                     <NavItem>
+                        <NavLinks>
+                           About
+                        </NavLinks>
+                     </NavItem>
+                     <NavItem>
+                        <NavLinks>
+                           Services
+                        </NavLinks>
+                     </NavItem>
+                     <NavItem>
+                        <NavLinks>
+                           Sign Up
+                        </NavLinks>
+                     </NavItem>
+                  </NavMenu>
+
+                  <NavBtn>
+                     <NavBtnLink to="/signin">Login</NavBtnLink>
+                  </NavBtn>
+
+               </NavbarContainer>
+            </Nav>
+         </IconContext.Provider>
+      </>
+   )
+}
+
+export default Navbar;
---
+++ src/pages/index.js
@@ -0,0 +1,22 @@
+
+
+import React, { useState } from 'react';
+import Navbar from '../components/Navbar';
+
+const Home = () => {
+   const [isOpen, setIsOpen] = useState(false);
+
+   const toggle = () => {
+      setIsOpen(!isOpen);
+   }
+
+
+   return (
+      <>
+         <Navbar toggle={toggle} />
+      </>
+   )
+
+};
+
+export default Home;
